b083360af057f52db1d89697ced41f00c0ab7516
sergiomenurt <smendezurtaza@danielcastelao.org>
2021-10-13 00:39:48 +0200

    Boletin2

---
+++ src/com/programacion/boletin2/CuentaAtras.java
@@ -0,0 +1,7 @@
+package com.programacion.boletin2;
+
+public class CuentaAtras {
+    public static void main(String[] args) {
+        System.out.println("Faltan 30 segundos para la cuenta atras");
+    }
+}
